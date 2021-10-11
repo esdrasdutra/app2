9c09d0cbecbf8e4bb519c01be46ef0ac0cf9d271
Dutra <esdras.dutra@capgemini.com>
2021-10-11 09:12:05 -0300

    Starting Versioning

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertaService } from "../../services/oferta.service";
+import { Oferta } from "src/app/shared/oferta.model";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [OfertaService]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas!: Oferta []
+
+  constructor(private ofertasService: OfertaService) { }
+
+  ngOnInit(): void {
+    this.ofertas = this.ofertasService.getOfertas()
+    console.log(this.ofertas)
+  }
+
+}
